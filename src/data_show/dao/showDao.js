435e8caaeae175b05d0e70a1f968e49b07b43db6
unknown <wendemo@163.com>
2015-05-24 16:38:37 +0800

    Signed-off-by: unknown <wendemo@163.com>

--- src/data_show/dao/showDao.js
+++ src/data_show/dao/showDao.js
@@ -10,9 +10,9 @@ exports.getData = function(range, callback){
 		   return;
 		}
 		
-		connection.query( 'SELECT t2.name, t1.pid, t1.device_id, t1.result, t1.unit, t1.institutions, t1.check_name, t1.case_no, ' +
-		 'date_format(t1.create_date, \'%Y-%m-%d %H:%i:%s\') as \'create_date\' FROM patient_result t1, patient_info t2 ' +
-		 ' WHERE t1.pid = t2.pid AND t1.device_id = t2.device_id;', function(err, rows) {
+		connection.query( 'SELECT t2.name, t1.pid, t1.device_id, t1.result, t3.name as \'unit\', t1.institutions, t4.name as \'check_type\', t1.case_no, ' +
+		 'date_format(t1.create_date, \'%Y-%m-%d %H:%i:%s\') as \'create_date\' FROM patient_result t1, patient_info t2, unit_value t3, check_type t4' +
+		 ' WHERE t1.pid = t2.pid AND t1.device_id = t2.device_id AND t1.unit = t3.id AND t1.check_type = t4.id;', function(err, rows) {
 		    // And done with the connection.
 			if (err) {
 			   console.log(err); 
--- src/data_show/service/data_query.js
+++ src/data_show/service/data_query.js
@@ -3,8 +3,8 @@ var dataDao = require('../dao/showDao')
 exports.DataQuery = function(req, res){
     range = req.body;
     
-    dataDao.getData(range, function(rows){
-	        res.send(JSON.stringify(rows));
+    dataDao.getData(range, function(rows){        	
+	    res.send(JSON.stringify(rows));
 	});
 }
 
@@ -14,6 +14,6 @@ exports.UserQuery = function(req, res){
 	var range = [Number(req.query.pid), req.query.device_id];
     
     dataDao.getUser(range, function(rows){
-	        res.send(JSON.stringify(rows));
+	    res.send(JSON.stringify(rows));
 	});
 }
